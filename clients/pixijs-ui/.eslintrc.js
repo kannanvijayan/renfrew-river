1d481bce7d6c5ced12e070ed1a867f8a2fbd853d
Kannan Vijayan <kv-github@kvijayan.com>
2023-11-05 21:28:34 -0500

    Newlines at end of files.
    Added eslint stylistic plugin, enabled ensuring files end with
    newlines, and fixed all of those issues.

--- clients/pixijs-ui/.eslintrc.js
+++ clients/pixijs-ui/.eslintrc.js
@@ -9,8 +9,9 @@ module.exports = {
     'no-unused-vars': 'off',
     '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
     '@typescript-eslint/no-explicit-any': 'off',
+    '@stylistic/eol-last': ['error', 'always'],
   },
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', '@stylistic', '@stylistic/ts'],
   root: true,
 }
