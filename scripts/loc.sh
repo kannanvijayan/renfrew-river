bce38f8f7e53d1b0e9abe5d3e98e91a041dd0529
Kannan Vijayan <kv-github@kvijayan.com>
2023-11-07 20:47:46 -0500

    Make loc.sh count protocol client lines.
    Now that protocol client has been split out into its own package.

--- scripts/loc.sh
+++ scripts/loc.sh
@@ -8,12 +8,14 @@ count_loc() {
 
 RUST_LINES=$(count_loc ./src '*.rs')
 SHADER_LINES=$(count_loc ./src '*.wgsl')
+PROTOCOL_CLIENT_LINES=$(count_loc ./clients/protocol-client/src '*.ts')
 PIXIJS_UI_LINES=$(count_loc ./clients/pixijs-ui/src '*.ts')
 
-TOTAL=$(($RUST_LINES + $SHADER_LINES + $PIXIJS_UI_LINES))
+TOTAL=$(($RUST_LINES + $SHADER_LINES + $PROTOCOL_CLIENT_LINES + $PIXIJS_UI_LINES))
 
 echo "Rust: $RUST_LINES"
 echo "Shader: $SHADER_LINES"
+echo "Protocol Client: $PROTOCOL_CLIENT_LINES"
 echo "PixiJS UI: $PIXIJS_UI_LINES"
 echo "========"
 echo "Total: $TOTAL"
