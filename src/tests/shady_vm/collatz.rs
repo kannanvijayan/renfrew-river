03411ab2c36560956ac0f3b9a6b99424cb9a29ce
Kannan Vijayan <kv-github@kvijayan.com>
2024-12-30 00:05:46 -0500

    Found more bugs.
    Expanded collatz test to report the number of steps
    and the result is wrong.
    
    Need to fix next.

--- src/tests/shady_vm/collatz.rs
+++ src/tests/shady_vm/collatz.rs
@@ -23,35 +23,38 @@ fn collatz_vmid() {
     // main: SET reg0 = (VM_ID % 5) + 1
     /* 0 */ asm.emit_mod(asm.dreg(0), asm.sreg_vmid(), asm.immv(5));
     /* 1 */ asm.emit_add(asm.dreg(0), asm.sreg(0), asm.immv(500));
+    /* 2 */ asm.emit_load(asm.dreg(2), 0);
 
     asm.bind_label("loop_start");
-    /* 2 */ asm.emit_sub(asm.dreg(99), asm.sreg(0), asm.immv(1));
-    /* 3 */ asm.with_ifz().emit_jump("loop_end");
+    /* 3 */ asm.emit_sub(asm.dreg(99), asm.sreg(0), asm.immv(1));
+    /* 4 */ asm.with_ifz().emit_jump("loop_end");
     // main: CALL collatz_step()
-    /* 4 */ asm.emit_call("collatz_step");
-    /* 5 */ asm.emit_jump("loop_start");
+    /* 5 */ asm.emit_call("collatz_step");
+    /* 6 */ asm.emit_add(asm.dreg(2), asm.sreg(2), asm.immv(1));
+    /* 7 */ asm.emit_jump("loop_start");
     asm.bind_label("loop_end");
     // main: END
-    /* 6 */ asm.emit_terminate();
+    /* 8 */ asm.emit_mov(asm.dreg(0), asm.sreg(2));
+    /* 9 */ asm.emit_terminate();
 
     // FUNC collatz_step() {
     asm.bind_label("collatz_step");
     asm.declare_label("collatz_step_even");
     asm.declare_label("collatz_ret");
     // collatz_step: IF reg0 % 2 == 1
-    /* 7 */ asm.emit_mod(asm.dreg(1), asm.sreg(0), asm.immv(2));
-    /* 8 */ asm.with_ifz().emit_jump("collatz_step_even");
+    /* 10 */ asm.emit_mod(asm.dreg(1), asm.sreg(0), asm.immv(2));
+    /* 11 */ asm.with_ifz().emit_jump("collatz_step_even");
     // collatz_step: THEN reg0 = reg0 * 3 + 1
-    /* 9 */ asm.emit_mul(asm.dreg(1), asm.sreg(0), asm.immv(3));
-    /* 10 */ asm.emit_add(asm.dreg(1), asm.sreg(1), asm.immv(1));
-    /* 11 */ asm.emit_jump("collatz_ret");
+    /* 12 */ asm.emit_mul(asm.dreg(1), asm.sreg(0), asm.immv(3));
+    /* 13 */ asm.emit_add(asm.dreg(1), asm.sreg(1), asm.immv(1));
+    /* 14 */ asm.emit_jump("collatz_ret");
     // collatz_step: ELSE reg0 = reg0 / 2
     asm.bind_label("collatz_step_even");
-    /* 12 */ asm.emit_div(asm.dreg(1), asm.sreg(0), asm.immv(2));
+    /* 15 */ asm.emit_div(asm.dreg(1), asm.sreg(0), asm.immv(2));
     // collatz_step: END
     asm.bind_label("collatz_ret");
-    /* 13 */ asm.emit_mov(asm.dreg(0), asm.sreg(1));
-    /* 14 */ asm.emit_ret();
+    /* 16 */ asm.emit_mov(asm.dreg(0), asm.sreg(1));
+    /* 17 */ asm.emit_ret();
     // }
   });
 
@@ -81,7 +84,18 @@ fn collatz_vmid() {
     let copy_regfiles = register_file_buffer.read_mappable_full_copy(&device).await;
     let regfiles = copy_regfiles.to_vec().await;
     for (i, regfile) in regfiles.iter().enumerate() {
-      assert_eq!((i, regfile.read_reg(241)), (i, 6));
+      assert_eq!((i, regfile.read_reg(241)), (i, count_collatz_steps(500 + i as u32)));
     }
   });
+}
+
+
+fn count_collatz_steps(n: u32) -> i32 {
+  let mut steps = 0;
+  let mut n = n;
+  while n != 1 {
+    n = if n % 2 == 0 { n / 2 } else { 3 * n + 1 };
+    steps += 1;
+  }
+  steps
 }
