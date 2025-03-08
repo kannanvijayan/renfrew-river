c7686347972cb65aa6c3b395b637e605b5d3e3a3
Kannan Vijayan <kv-github@kvijayan.com>
2025-03-08 17:18:53 -0500

    More work on rendering
    Not actually rendering real data yet, just verifying the hex-tile mapping
    logic in the shader works as expected using manually constructed buffers.
    
    The hex-tile mapping algorithms in the shader seem fine now, but the lookups
    against textures still have weird off-by-one issues.

--- clients/web-client/src/graphics/hex_mesh.ts
+++ clients/web-client/src/graphics/hex_mesh.ts
@@ -26,11 +26,57 @@ export default class HexMesh {
 function makeTexture(): PIXI.Texture {
   // Make an array-buffer texture.
   const textureData = new Float32Array(1024 * 1024 * 4);
-  for (let i = 0; i < textureData.length; i++) {
-    if (i % 4 != 3) {
-      textureData[i] = Math.random();
-    } else {
-      textureData[i] = 1;
+  for (let i = 0; i < (1024 * 1024); i++) {
+    const row = Math.floor(i / 1024);
+    // For the first 10 rows, switch between black and green.
+    if (row < 10 || (row >= (1024 - 10))) {
+      if (row % 2 === 0) {
+        textureData[i * 4 + 0] = 0.0;
+        textureData[i * 4 + 1] = 0.0;
+        textureData[i * 4 + 2] = 0.0;
+        textureData[i * 4 + 3] = 1.0;
+      } else {
+        textureData[i * 4 + 0] = 0.0;
+        textureData[i * 4 + 1] = 1.0;
+        textureData[i * 4 + 2] = 0.0;
+        textureData[i * 4 + 3] = 1.0;
+      }
+      continue;
+    }
+    // For the first 50 rows, switch between red and yellow.
+    if (row < 50 || (row >= (1024 - 50))) {
+      if (row % 2 === 0) {
+        textureData[i * 4 + 0] = 1.0;
+        textureData[i * 4 + 1] = 0.0;
+        textureData[i * 4 + 2] = 0.0;
+        textureData[i * 4 + 3] = 1.0;
+      } else {
+        textureData[i * 4 + 0] = 1.0;
+        textureData[i * 4 + 1] = 1.0;
+        textureData[i * 4 + 2] = 0.0;
+        textureData[i * 4 + 3] = 1.0;
+      }
+      continue;
+    }
+    switch (i % 3) {
+      case 0:
+        textureData[i * 4 + 0] = 1.0;
+        textureData[i * 4 + 1] = 0.0;
+        textureData[i * 4 + 2] = 0.0;
+        textureData[i * 4 + 3] = 1.0;
+        break;
+      case 1:
+        textureData[i * 4 + 0] = 0.0;
+        textureData[i * 4 + 1] = 1.0;
+        textureData[i * 4 + 2] = 0.0;
+        textureData[i * 4 + 3] = 1.0;
+        break;
+      case 2:
+        textureData[i * 4 + 0] = 0.0;
+        textureData[i * 4 + 1] = 0.0;
+        textureData[i * 4 + 2] = 1.0;
+        textureData[i * 4 + 3] = 1.0;
+        break;
     }
   }
   return PIXI.Texture.fromBuffer(textureData, 1024, 1024, {
@@ -60,6 +106,7 @@ function makeShader(screenSize: [number, number], texMapData: PIXI.Texture)
   console.log("Setting screen size", screenSize);
   return PIXI.Shader.from(VERTEX_SHADER, FRAGMENT_SHADER, {
     uScreenSize: screenSize,
+    uMapCellWidth: 10,
     texMapData,
   });
 }
@@ -77,26 +124,89 @@ const VERTEX_SHADER = `
     vec2 axisFlipped = vec2(leftAligned.x, -leftAligned.y);
     vec2 topAligned = axisFlipped + vec2(0.0, 1.0);
     vec2 unitScaled = topAligned * 0.5;
-    vec2 screenPos = unitScaled;
     gl_Position = vec4(aPosition, 0.0, 1.0);
-    vPos = screenPos;
+    vPos = unitScaled;
   }
 `;
 
-const FRAGMENT_SHADER = `
-  uniform vec2 uScreenSize;
+const HEXGRID_LIB = `
+  const vec2 cNormalScaleCell = vec2(1.0, 1.0);
+  const vec2 cNormalScaleMul = vec2(0.75, 1.0);
 
-  uniform sampler2D texMapData;
-
-  varying vec2 vPos;
+  const float cEpsilon = 0.0001;
+  float isZero(float x) {
+    if (abs(x) < cEpsilon) {
+      return 1.0;
+    } else {
+      return 0.0;
+    }
+  }
 
   float fmod(float a, float b) {
     return a - b * floor(a / b);
   }
 
-  void main() {
-    vec2 pos = vPos * uScreenSize;
+  vec2 hexCellUnderNormalOffset(vec2 normalOffset) {
+    float bbCol = floor(normalOffset.x / cNormalScaleMul.x);
+    float oddColumn = fmod(bbCol, 2.0);
+    float columnShift = oddColumn * (cNormalScaleCell.y / 2.0);
+    float bbRow = floor((normalOffset.y - columnShift) / cNormalScaleMul.y);
+    vec2 bb = vec2(bbCol, bbRow);
 
+    vec2 offset = (
+       normalOffset
+         - (bb * cNormalScaleMul)
+         - vec2(0.0, columnShift + (cNormalScaleCell.y / 2.0))
+    );
+
+    float slope = (cNormalScaleCell.x / 4.0) / (cNormalScaleCell.y / 2.0);
+    float ratio = offset.x / offset.y;
+
+    if (offset.y < 0.0) {
+      // Top half.
+      if (ratio >= -slope) {
+        bb = bb - vec2(1.0, 1.0 - oddColumn);
+      }
+    } else {
+      // Bottom half.
+      if (ratio < slope) {
+        bb = bb - vec2(1.0, oddColumn * -1.0);
+      }
+    }
+    return bb;
+  }
+`;
+
+const FRAGMENT_SHADER = `
+  ${HEXGRID_LIB}
+
+  uniform vec2 uScreenSize;
+  uniform float uMapCellWidth;
+
+  uniform sampler2D texMapData;
+
+  varying vec2 vPos;
+
+  void main() {
+    vec2 normPos = (vPos * uScreenSize) / uMapCellWidth;
+    vec2 hexCell = hexCellUnderNormalOffset(normPos);
+    vec2 texCoord = (hexCell * vec2(1.0, -1.0)) + vec2(0.0, 1024.0);
+    vec4 color2 = texture2D(texMapData, texCoord / 1024.0);
+    gl_FragColor = vec4(color2.xyz, 1.0);
+
+    if (vPos.x > 0.5) {
+      vec4 color3;
+      if (hexCell.y < 0.0) {
+        color3 = vec4(1.0, 1.0, 1.0, 1.0);
+      } else if (hexCell.y < 1.0) {
+        color3 = vec4(1.0, 0.0, 0.0, 1.0);
+      } else {
+        color3 = texture2D(texMapData, vec2(0.5, 0.5) / 1024.0);
+      }
+      gl_FragColor = color3;
+    }
+
+    /*
     float xf = fmod(pos.x, 100.0);
     float yf = fmod(pos.y, 100.0);
     float bchan = 0.0;
@@ -113,12 +223,13 @@ const FRAGMENT_SHADER = `
     float dy = pos.y - 100.0;
     float dist = sqrt(dx * dx + dy * dy);
     if (dist < 50.0) {
-      vec4 color = texture2D(texMapData, vec2(dx + 50.0, dy + 50.0) / 1000.0);
+      vec4 color = texture2D(texMapData, (vec2(dx + 50.0, dy + 50.0) / 1000.0));
       gl_FragColor = color;
     }
 
     if (uScreenSize.y - pos.y < 50.0) {
       gl_FragColor = vec4(0.8, 0.7, 0.3, 1.0);
     }
+    */
   }
 `;
